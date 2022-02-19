5abc0ad731277df0f442f60843fd8360f508c0d2
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-02-19 10:00:54 +0100

    Update settings.py

--- workers/settings.py
+++ workers/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,13 +82,13 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql',
 
-        'NAME': 'vxuccnnl',
+        'NAME': os.environ.get('NAME'),
 
-        'USER': 'vxuccnnl',
+        'USER': os.environ.get('USER'),
 
-        'PASSWORD': 'P6NsNb1y5K0bXozX0DucP7cohO-KqBeh',
+        'PASSWORD': os.environ.get('PASSWORD'),
 
-        'HOST': 'abul.db.elephantsql.com',
+        'HOST': os.environ.get('HOST'),
 
         'PORT': '5432',
 
